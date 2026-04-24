de728d40f68cf921e25f55fb70818a6eac98cfbe
Lukas <lukasassfall@gmail.com>
2026-04-24 15:17:36 +0200

    commit19

--- obj/Debug/net8.0/FFRastenfeld.AssemblyInfo.cs
+++ obj/Debug/net8.0/FFRastenfeld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FFRastenfeld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b3f6489ef556b80e405e2f04d7959ae63c50c6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57c61aa6ca9d9a63e35ac0202513019996508470")]
 [assembly: System.Reflection.AssemblyProductAttribute("FFRastenfeld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FFRastenfeld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

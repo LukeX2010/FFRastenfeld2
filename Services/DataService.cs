99025a8cb9ceca0d2d3ee30f1194de1c760beae1
LukeX2010 <lukasassfall@gmail.com>
2026-05-31 20:48:17 +0200

    commit27

--- Services/DataService.cs
+++ Services/DataService.cs
@@ -174,8 +174,8 @@ public class DataService
     new Post
     {
         Id = 8,
-        Slug = "test2",
-        Emoji = "test2",
+        Slug = "keinbild",
+        Emoji = "keinbild",
         Titel = "GMA-Alarm im Hotel Ottenstein",
         Kategorie = "Einsätze",
         Datum = new DateTime(2025, 12, 20, 23, 50, 0),
@@ -274,32 +274,39 @@ public class DataService
 // ── MITGLIEDER ──────────────────────────────────────────────────────────
     public List<Mitglied> GetMitglieder() => new()
 {
-    new Mitglied { Id=1, Name="Matthias Goll", Dienstgrad="Hauptbrandinspektor", Funktion="Kommandant", EintriebAb=2005, IstFuehrung=true },
-    new Mitglied { Id=2, Name="Stefan Schulmeister", Dienstgrad="Brandinspektor", Funktion="Kommandant-Stv.", EintriebAb=2008, IstFuehrung=true },
+    new Mitglied { Id=1, Name="Matthias Goll", Dienstgrad="Hauptbrandinspektor", Funktion="Kommandant", EintriebAb=2019, IstFuehrung=true },
+    new Mitglied { Id=2, Name="Stefan Schulmeister", Dienstgrad="Brandinspektor", Funktion="Kommandant-Stv.", EintriebAb=2011, IstFuehrung=true },
 
-    new Mitglied { Id=3, Name="Gregor Dastel", Dienstgrad="Verwaltungsmeister", Funktion="Stv. Leiter des Verwaltungsdienstes", EintriebAb=2010, IstFuehrung=true },
-
-    new Mitglied { Id=4, Name="Richard Kröpfl", Dienstgrad="Brandinspektor", Funktion="Zugskommandant", EintriebAb=2009, IstFuehrung=true },
+    new Mitglied { Id=3, Name="Gregor Dastel", Dienstgrad="Verwaltungsmeister", Funktion="Stv. Leiter des Verwaltungsdienstes", EintriebAb=1998, IstFuehrung=true },
+    new Mitglied { Id=4, Name="Erwin Huber", Dienstgrad="Verwaltungsmeister", Funktion="Stv. Leiter des Verwaltungsdienstes", EintriebAb=2010, IstFuehrung=true },
 
     new Mitglied { Id=5, Name="Johann Herzog jun.", Dienstgrad="Oberbrandmeister", Funktion="1. Gruppenkommandant", EintriebAb=2012, IstFuehrung=true },
     new Mitglied { Id=6, Name="Emanuel Huber", Dienstgrad="Oberbrandmeister", Funktion="2. Gruppenkommandant / Zeugmeister", EintriebAb=2011, IstFuehrung=true },
-    new Mitglied { Id=7, Name="Max Artner", Dienstgrad="Oberbrandmeister", Funktion="3. Gruppenkommandant", EintriebAb=2013, IstFuehrung=true },
+    new Mitglied { Id=7, Name="Max Artner", Dienstgrad="Oberbrandmeister", Funktion="3. Gruppenkommandant", EintriebAb=2015, IstFuehrung=true },
 
     new Mitglied { Id=8, Name="Alexander Gassner", Dienstgrad="Löschmeister", Funktion="Fahrmeister", EintriebAb=2015, IstFuehrung=true },
-
-    new Mitglied { Id=9, Name="Hannes Auer", Dienstgrad="Löschmeister", Funktion="Sachbearbeiter Nachrichtendienst", EintriebAb=2016, IstFuehrung=true },
+    new Mitglied { Id=9, Name="Richard Kröpfl", Dienstgrad="Brandinspektor", Funktion="Zugskommandant", EintriebAb=1998, IstFuehrung=true },
     new Mitglied { Id=10, Name="Patrick Kirchberger", Dienstgrad="Löschmeister", Funktion="Sachbearbeiter Vorbeugender Brandschutz", EintriebAb=2017, IstFuehrung=true },
     new Mitglied { Id=11, Name="Marina Kröpfl", Dienstgrad="Löschmeister", Funktion="Sachbearbeiter Öffentlichkeitsarbeit", EintriebAb=2018, IstFuehrung=true },
-    new Mitglied { Id=12, Name="Matthias Goll", Dienstgrad="Hauptbrandinspektor", Funktion="Sachbearbeiter Schadstoffdienst", EintriebAb=2005, IstFuehrung=true },
+    new Mitglied { Id=12, Name="Bernhard Traxler", Dienstgrad="Hauptbrandinspektor", Funktion="", EintriebAb=2005, IstFuehrung=true },
 
     new Mitglied { Id=13, Name="Johann Herzog sen.", Dienstgrad="Hauptlöschmeister", Funktion="Sachbearbeiter Feuerwehrgeschichte", EintriebAb=1995, IstFuehrung=true },
-    new Mitglied { Id=14, Name="Stefan Schulmeister", Dienstgrad="Brandinspektor", Funktion="Sachbearbeiter Wasserdienst", EintriebAb=2008, IstFuehrung=true },
+    new Mitglied { Id=14, Name="Christian Frühwirt", Dienstgrad="Brandinspektor", Funktion="Sachbearbeiter Wasserdienst", EintriebAb=2008, IstFuehrung=true },
 
     new Mitglied { Id=15, Name="Lukas Assfall", Dienstgrad="Feuerwehrmann", Funktion="Sachbearbeiter EDV / Informationstechnologie", EintriebAb=2025, IstFuehrung=true },
     new Mitglied { Id=16, Name="Carmen Goll", Dienstgrad="Feuerwehrfrau", Funktion="Sachbearbeiter Feuerwehrmedizinischer Dienst", EintriebAb=2019, IstFuehrung=true },
     new Mitglied { Id=17, Name="Felix Dornahckl", Dienstgrad="Feuerwehrmamnn", Funktion="", EintriebAb=2024, IstFuehrung=true },
     new Mitglied { Id=18, Name="Jakob Assfall", Dienstgrad="Feuerwehrmann", Funktion="", EintriebAb=2025, IstFuehrung=true },
-    new Mitglied { Id=16, Name="Victoria Lenz", Dienstgrad="Feuerwehrfrau", Funktion="", EintriebAb=2024, IstFuehrung=true },
+    new Mitglied { Id=16, Name="Victoria Lenz", Dienstgrad="Feuerwehrfrau", Funktion="Sachbearbeiter Öffentlichkeitsarbeit", EintriebAb=2024, IstFuehrung=true },
+    new Mitglied { Id=17, Name="Hannes Auer", Dienstgrad="Löschmeister", Funktion="Sachbearbeiter Nachrichtendienst", EintriebAb=2016, IstFuehrung=true },
+    new Mitglied { Id=18, Name="Gerhard Steininger", Dienstgrad="Oberfeuerwehrmann", Funktion="", EintriebAb=2010, IstFuehrung=true },
+    new Mitglied { Id=19, Name="Thomas Hofbauer", Dienstgrad="Oberfeuerwehrmann", Funktion="", EintriebAb=2018, IstFuehrung=true },
+    new Mitglied { Id=20, Name="Reinhard Hasengst", Dienstgrad="Oberfeuerwehrmann", Funktion="", EintriebAb=2018, IstFuehrung=true },
+    new Mitglied { Id=21, Name="Walter Göschl", Dienstgrad="Oberfeuerwehrmann", Funktion="", EintriebAb=2018, IstFuehrung=true },
+    new Mitglied { Id=22, Name="Wolfgang Göschl", Dienstgrad="Oberfeuerwehrmann", Funktion="", EintriebAb=1982, IstFuehrung=true },
+    new Mitglied { Id=23, Name="Franz Hofmann", Dienstgrad="Oberfeuerwehrmann", Funktion="", EintriebAb=1982, IstFuehrung=true },
+    new Mitglied { Id=24, Name="Herbert Weitl", Dienstgrad="Oberfeuerwehrmann", Funktion="", EintriebAb=1990, IstFuehrung=true },
+    new Mitglied { Id=25, Name="Robert Zinner", Dienstgrad="Oberfeuerwehrmann", Funktion="", EintriebAb=1990, IstFuehrung=true },
     
 };
     
